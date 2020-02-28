a81b3a71c2bc03d8e5cd68f45def5a917670854e
Luiz Pereira <luizfper@gmail.com>
2020-02-28 11:14:27 -0500

    done

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
 		get 'api/posts/refresh' => 'posts#update_post_list'
 		post 'api/posts/test' => 'posts#test'
 
-		get 'api/posts/all' => 'posts#all_posts'
+		get 'api/posts/all' => 'posts#all_post	s'
+
+		post 'api/posts/keep_awake' => 'posts#update_post_list'
 
 end
 # JSON.parse('https://hacker-news.firebaseio.com/v0/newstories.json')
